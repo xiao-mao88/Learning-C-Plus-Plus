26a41406a52e72171911add204f4c95ce8e231a0
xiao_mao <laurenli045@gmail.com>
2023-02-08 23:31:47 -0500

    my makeshift business card

---
+++ 001BusinessCard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+std::cout << "*************\n";
+std::cout << "* Lauren Li *\n";
+std::cout << "*  Student  *\n";
+std::cout << "*************\n";
+}
