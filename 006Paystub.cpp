e9d9dd2bf415bf8f0383381b80839d98354e81d4
xiao_mao <laurenli045@gmail.com>
2023-02-10 00:13:43 -0500

    a hypothetical paystub

---
+++ 006Paystub.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double payRate=30, hours=40, Grosspay=hours*payRate;
+  string name= "Lauren Li";
+  cout<< "-----------------------\n| Employee: "<<name<<" |\n| Pay Rate: "<<payRate <<"        |\n| Hours: "<<hours <<"           |\n| Gross Pay: "<<Grosspay<<"     |"<< "\n-----------------------";
+}
