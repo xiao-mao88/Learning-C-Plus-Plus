285b0dd3c3f0160735b8b2f05162fa26d47c8857
xiao_mao <laurenli045@gmail.com>
2023-02-10 18:15:09 -0500

    rounds pi to various decimal places; uses setprecision

---
+++ 008RoundingPi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+  const double pi=3.14159265;
+  cout << fixed << setprecision(8) << pi << endl;
+  cout<< setprecision(7) << pi << endl;
+  cout<< setprecision(2) << pi << endl;
+
+}
