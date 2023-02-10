be5cbefd82c2b0c7ccaa75cbb43c380b45948558
xiao_mao <laurenli045@gmail.com>
2023-02-10 00:02:29 -0500

    just a hanging stick figure

---
+++ 003Hangman.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "   ----------\n   |        |\n   O        |\n  /I\\       |\n  | |       |\n            |\n   ---------|--\n";
+  
+}
