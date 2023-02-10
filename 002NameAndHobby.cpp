941093a773ed68f308f9da2f9f7af6ca88384d42
xiao_mao <laurenli045@gmail.com>
2023-02-09 23:58:33 -0500

    my name and hobby

---
+++ 002NameAndHobby.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  string name = "Lauren";
+  string hobby = "draw";
+  cout << "My name is " << name << endl;
+  cout << "I like to " << hobby;
+
+}
