b44f4dd4fc0ddc83d64849daf898559ca798443b
xiao_mao <laurenli045@gmail.com>
2023-02-26 12:00:16 -0500

    converts a number from 1-1000 into roman numerals

---
+++ 032RomanNumerals.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num, hundreds, tens, ones;
+  cout<<"Please pick a number from 1-1000."<<endl;
+  cin>>num;
+  if (num==1000)
+  {
+    cout<<"Roman Numeral: M";
+  }
+  else 
+  {
+    cout<<"Roman Numeral: ";
+  }
+
+  hundreds=num/100;
+  switch (hundreds)
+  {
+    case 1:
+    cout<<"C ";
+    break;
+
+    case 2:
+    cout<<"CC ";
+    break;
+
+    case 3:
+    cout<<"CCC ";
+    break;
+
+    case 4:
+    cout<<"CD ";
+    break;
+
+    case 5:
+    cout<<"D ";
+    break;
+
+    case 6:
+    cout<<"DC ";
+    break;
+
+    case 7:
+    cout<<"DCC ";
+    break;
+
+    case 8:
+    cout<<"DCCC ";
+    break;
+
+    case 9:
+    cout<<"CM ";
+  }
+
+  tens=num/10%10;
+  switch(tens)
+  {
+    case 1:
+    cout<<"X ";
+    break;
+
+    case 2:
+    cout<<"XX ";
+    break;
+
+    case 3:
+    cout<<"XXX ";
+    break;
+
+    case 4:
+    cout<<"XL ";
+    break;
+
+    case 5:
+    cout<<"L ";
+    break;
+
+    case 6:
+    cout<<"LX ";
+    break;
+
+    case 7:
+    cout<<"LXX ";
+    break;
+
+    case 8:
+    cout<<"LXXX ";
+    break;
+
+    case 9:
+    cout<<"XC ";
+    break;
+  }
+
+  ones=num%10;
+  switch (ones)
+  {
+    case 1:
+    cout<<"I ";
+    break;
+
+    case 2:
+    cout<<"II ";
+    break;
+
+    case 3:
+    cout<<"III ";
+    break;
+
+    case 4:
+    cout<<"IV ";
+    break;
+
+    case 5:
+    cout<<"V ";
+    break;
+
+    case 6:
+    cout<<"VI ";
+    break;
+
+    case 7:
+    cout<<"VII ";
+    break;
+
+    case 8:
+    cout<<"VIII ";
+    break;
+
+    case 9:
+    cout<<"IX ";
+    break;
+  }
+}
