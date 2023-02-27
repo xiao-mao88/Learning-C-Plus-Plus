458e007a0cf7a8d5e1aef40ef6f322336022518b
xiao_mao <laurenli045@gmail.com>
2023-02-26 23:28:37 -0500

    different powers of an inputed number

---
+++ 033PowersOfX.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double num;
+  cout<<"Please enter a number."<<endl;
+  cin>>num;
+  cout<<endl;
+  
+for(int i = 1; i<=10; i++)
+	{
+		cout << pow(num,i) << endl;
+	}
+
+
+}
