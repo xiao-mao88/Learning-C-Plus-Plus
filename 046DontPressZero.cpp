51c2d05d4692e379797a4c5f4668703532e3ee0b
xiao_mao <laurenli045@gmail.com>
2023-03-03 21:41:31 -0500

    don git status

---
+++ 046DontPressZero.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+int main() {
+  int num, answer;
+  cout<<"Please enter a number greater than zero: ";
+  cin>>num;
+  while(num>0)
+  {
+    answer=num%10;
+    switch(answer)
+    {
+      case 1:
+      cout<<"You're a cool person for following directions!"<<endl<<endl;
+      break;
+
+      case 2:
+      cout<<"Good job following directions"<<endl<<endl;
+      break;
+
+      case 3:
+      cout<<"You're awesome for following directions!"<<endl<<endl;
+      break;
+
+      case 4:
+      cout<<"You are a good child for following directions!"<<endl<<endl;
+      break;
+
+      case 5:
+      cout<<"OwO"<<endl<<endl;
+      break;
+
+      case 6:
+      cout<<"Great job! You followed directions!."<<endl<<endl;
+      break;
+
+      case 7:
+      cout<<"How's it like being the cool kid? :)"<<endl<<endl;
+      break;
+
+      case 8:
+      cout<<"Whoooohoooo! Go again!"<<endl<<endl;
+      break;
+
+      case 9:
+      cout<<"Yaaaayyyyy! You are awesome at following directions."<<endl<<endl;
+      break;
+
+      default:
+      cout<<"Thanks for following directions! You get to play again!"<<endl<<endl;
+      break;
+    }
+    
+    cout<<"Please enter a number greater than zero: ";
+    cin>>num;
+  }
+  if(num<=0)
+  {
+    cout<<endl<<"Why you no follow directions! Restart and try again. / ಠ_ಠ /";
+  }
+}
