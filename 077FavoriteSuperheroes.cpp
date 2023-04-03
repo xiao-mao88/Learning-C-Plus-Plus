5a07bd040fabbeb155b36f346585af2a8d6d25e8
xiao_mao <laurenli045@gmail.com>
2023-04-02 21:38:40 -0400

    printing strings from array

---
+++ 077FavoriteSuperheroes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string superhero[5]={"1. Iron Man", "2. Deku", "3. Saitama", "4. Todoroki", "5. Black Widow"};
+  for(int i=0;i<5;i++)
+  {
+    cout<<superhero[i]<<endl;
+  }
+}
